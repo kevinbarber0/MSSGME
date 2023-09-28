b287b00226cd7e795a538d9bfd3c6a71b65b5a7a
Kevin <kevinbarber0@outlook.com>
2023-09-28 18:42:10 -0400

    iPhoneintegration

--- text.js
+++ text.js
@@ -15,4 +15,34 @@ async function sendSMS() {
         .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
 }
 
-sendSMS();
+////////
+function scrollDown(){
+  var focusBottom = document.getElementById("adobewordpress");
+  focusBottom.scrollTop = focusBottom.scrollHeight;
+}
+ 
+
+
+$('form.chat input[type="submit"]').click(function(event){
+  event.preventDefault(); 
+  var message = $('form.chat input[type="text"]').val(); 
+  if( $('form.chat input[type="text"]').val()) { 
+    var d = new Date(); 
+    var clock = d.getHours() + ":" + d.getMinutes();
+    var month = d.getMonth()+1;
+    var day = d.getDate();
+    var currentDate =
+        ((''+month).length<2 ? '0' : '') + month + '.' +
+        ((''+day).length<2 ? '0' : '') + day +'&nbsp;&nbsp;'+ clock;
+
+    $('form.chat div.messages').append('<div class="message"><div class="myMessage"><p>'+message+'</p><date>'+clock+'</date></div></div>');
+    setTimeout(function() {
+      $('form.chat > span').addClass('spinner'); 
+    }, 100);
+    setTimeout(function() {
+      $('form.chat > span').removeClass('spinner'); 
+    }, 2000);
+  }
+  $('form.chat input[type="text"]').val(''); 
+  scrollDown(); 
+});
